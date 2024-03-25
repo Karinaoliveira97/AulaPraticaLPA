c467c4d6d65959b7d2018b1bf090ade693b51990
KarinaOliveira <karinaoliveira097@gmail.com>
2024-03-25 15:52:04 -0300

    Signed-off-by: KarinaOliveira <karinaoliveira097@gmail.com>

---
+++ DiagramaClasseJogo/Entity.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+class Entity:
+    def __init__(self):
+        self.name = None
+        self.rect = None
+        self.surf = None
+
+    def move(self, ):
+        pass
+
